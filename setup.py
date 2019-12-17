97d60c35440241d1f0b59823fc3e51dafbfa9088
Ihor Liubymov <infunt@gmail.com>
2019-12-17 22:56:54 +0200

    added long_description

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='airflow_multi_dagrun',
-    version='1.0',
+    version='1.1',
     description='MultiDagRunPlugin for airflow',
     python_requires='>=3.6.0',
     author='Ihor Liubymov',
     author_email='infunt@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/mastak/airflow_multi_dagrun',
     entry_points={
         'airflow.plugins': [
